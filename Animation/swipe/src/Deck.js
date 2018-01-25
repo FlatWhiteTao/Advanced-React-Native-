0ff927d8f146969503ab09a68a11056de5819bbe
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-25 20:55:19 +1100

    Populate dummy data for tinder card component for testing purpose

---
+++ Animation/swipe/src/Deck.js
@@ -0,0 +1,12 @@
+import React, { Component } fro 'react';
+import { View, Animated } from 'react-native';
+
+class Deck extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+export default Deck;
